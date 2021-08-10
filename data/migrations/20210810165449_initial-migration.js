b11e36ac7903fca29bb7b0b1657ff7af116b746f
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-10 19:05:17 +0200

    wip

--- data/migrations/20210810165449_initial-migration.js
+++ data/migrations/20210810165449_initial-migration.js
@@ -1,9 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable('fruits', tbl => {
     tbl.increments('fruit_id') // creates an id column, auto inc integers, PKey!
-    tbl.text('fruit_name', 256).unique().notNullable()
+    tbl.text('fruit_name', 256).unique().notNullable() // required and unique
     tbl.float('avg_weight').notNullable() // required
-    tbl.boolean('is_delicious')
+    tbl.boolean('is_delicious').defaultTo(true) // if not provided defaults to tru
   });
 };
 
