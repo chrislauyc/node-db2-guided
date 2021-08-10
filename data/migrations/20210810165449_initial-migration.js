4d2ace42b91c9270412930e17db3d7af7b5dae87
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-10 19:06:03 +0200

    wip

--- data/migrations/20210810165449_initial-migration.js
+++ data/migrations/20210810165449_initial-migration.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     tbl.increments('fruit_id') // creates an id column, auto inc integers, PKey!
     tbl.text('fruit_name', 256).unique().notNullable() // required and unique
     tbl.float('avg_weight').notNullable() // required
-    tbl.boolean('is_delicious').defaultTo(true) // if not provided defaults to tru
+    tbl.boolean('is_delicious').defaultTo(true) // if not provided defaults to true
   });
 };
 
