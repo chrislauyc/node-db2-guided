c0a63dc83f52c88e8fac713d8a762419fc72873c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-10 19:25:53 +0200

    wip

--- data/seeds/fruits.js
+++ data/seeds/fruits.js
@@ -2,9 +2,9 @@ exports.seed = function (knex) { // dependency injection
   return knex('fruits').truncate() // reset primary keys
     .then(function () {
       return knex('fruits').insert([
-        { fruit_name: 'rowValue1', avg_weight:   }, // DELETE THE IDS!!!!!!!!!
-        { fruit_name: 'rowValue2', avg_weight:   },
-        { fruit_name: 'rowValue3', avg_weight:   }
+        { fruit_name: 'apple', avg_weight: 10.5, is_delicious: true  }, // DELETE THE IDS!!!!!!!!!
+        { fruit_name: 'rowValue2', avg_weight: 10.5, is_delicious: false  },
+        { fruit_name: 'rowValue3', avg_weight: 10.5 },
       ]);
     });
 };
