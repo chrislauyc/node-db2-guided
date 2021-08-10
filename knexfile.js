dda3afc8ddd07cd542414ee1e9487f9307429e85
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-10 18:35:35 +0200

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,9 @@
 module.exports = {
   // different configs for diffrent dbs (dev, prod, testing, qa, staging...)
+  development : {
+    client: 'sqlite3',
+    connection: {
+      filename: '',
+    }
+  }
 }
